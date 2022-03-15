4039185761678eda0451cd17576e6592bda01cd8
Matt E <matthew.entwistle@kidsloop.live>
2022-03-15 16:38:05 +0000

    add alert condition interface

--- src/model/nr-alerts.ts
+++ src/model/nr-alerts.ts
@@ -14,3 +14,29 @@ export interface AlertPolicyNerdGraph {
   incidentPreference: string;
   name: string;
 }
+
+export interface AlertConditionNerdGraph {
+  enabled: boolean;
+  id: string;
+  name: string;
+  nrql: {
+    query: string;
+  };
+  policyId: string;
+  runbookUrl?: string;
+  signal: {
+    aggregationDelay: number;
+    aggregationMethod: string;
+    aggregationWindow: number;
+  };
+  terms: [
+    {
+      operator: string;
+      priority: string;
+      threshold: number;
+      thresholdDuration: number;
+      thresholdOccurrences: string;
+    }
+  ];
+  type: string;
+}
